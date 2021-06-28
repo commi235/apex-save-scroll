79591647c159da280f5ab767b850ffea24950681
commi235 <moritz.klein@it-twins.de>
2021-06-28 13:46:27 +0200

    Prettier, eslint and instrumentation

--- src/js/apex_save_scroll.js
+++ src/js/apex_save_scroll.js
@@ -1,36 +1,34 @@
 /**
  * Utility Module to save and restore scroll position.
  */
-var apexScrollUtil = (
-  function( $, storage ) {
-    var _pubMethods    = {},
-        _storagePrefix = 'apexSaveScrollPos',
-        _itemPrefix    = 'scroll_',
-        _myStorage     = storage.getScopedSessionStorage
-                         ({
-                            prefix: _storagePrefix,
-                            useAppId: true
-                         });
-    
-    function _get(myPageId)
-    {
-      return _myStorage.getItem( _itemPrefix + myPageId );
-    }
+var apexScrollUtil = ( function ( $, storage, debug ) {
+  var _pubMethods = {},
+    _storagePrefix = "apexSaveScrollPos",
+    _itemPrefix = "scroll_",
+    _myStorage = storage.getScopedSessionStorage( {
+      prefix: _storagePrefix,
+      useAppId: true,
+    } );
 
-    _pubMethods.save = function(myPageId)
-    {
-      _myStorage.setItem( _itemPrefix + myPageId, $(window).scrollTop() );
-    }
+  function _get( myPageId ) {
+    debug.trace( `Get stored value for ${myPageId}` );
+    return _myStorage.getItem( _itemPrefix + myPageId );
+  }
 
-    _pubMethods.restore = function(myPageId)
-    {
-      $(window).scrollTop(_get(myPageId));  
-    }
+  _pubMethods.save = function ( myPageId ) {
+    debug.trace( `Storing scroll position for page ${myPageId}` );
+    _myStorage.setItem( _itemPrefix + myPageId, $( window ).scrollTop() );
+  };
 
-    _pubMethods.clear = function()
-    {
-      _myStorage.clear();
-    }
-  
-    return _pubMethods;
-  } ( apex.jQuery, apex.storage ) );
+  _pubMethods.restore = function ( myPageId ) {
+    debug.trace( `Scrolling to stored position for page ${myPageId}` );
+    $( window ).scrollTop( _get( myPageId ) );
+  };
+
+  _pubMethods.clear = function () {
+    debug.trace( `Clearing sorage` );
+    _myStorage.clear();
+  };
+
+  return _pubMethods;
+} )( apex.jQuery, apex.storage, apex.debug );
